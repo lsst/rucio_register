51a542a5c705171c27311d1ac3df89c3958510ff
Stephen R Pietrowicz <srp@illinois.edu>
2025-02-13 11:10:43 -0600

    Revert "Fix type check issue"
    This reverts commit 1db9a97f2bd6a28c0f0423dd92bd7bbdd36a4f84.

--- python/lsst/rucio/register/rucio_interface.py
+++ python/lsst/rucio/register/rucio_interface.py
@@ -301,7 +301,7 @@ class RucioInterface:
         """
         bundles = []
         for dataset_ref in dataset_refs:
-            if isinstance(dataset_ref, list):
+            if dataset_ref is list:
                 for dsr in dataset_ref:
                     logging.debug(f"{self.butler.getURI(dsr)=}")
                     logging.debug(f"{dsr.to_json()=}")
